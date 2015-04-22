36842011bba28f8c309f4c3694bc166c10277017
Tony McCallie <tony@threeleaf.net>
2015-04-22 15:46:01 -0500

    regex replace href with onclick

--- www/js/services.js
+++ www/js/services.js
@@ -60,6 +60,10 @@ myApp.service('Community', function ($http, $location) {
 			.success(function(response, status, headers, config) {
 				if(response.status === 'SUCCESS') {
 					angular.forEach(response.data, function(item) {
+						str = item.CommunityPost.body;
+						str = str.replace(/href=\"\//ig,'href="http://www.gracepointcoppell.org/');
+						str = str.replace(/src=\"\//ig,'src="http://www.gracepointcoppell.org/');
+						item.CommunityPost.body = str.replace(/href=\"(.+?)\"/gi,'onclick="window.open(\'$1\',\'_system\',\'location=yes\');"');
 						posts.push(item);
 					});
 				} else {
