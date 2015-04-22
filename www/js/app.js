7d3a549e30f8140aff4a1aafd224b516501c1505
Tony McCallie <tony@threeleaf.net>
2015-04-22 11:56:32 -0500

    mv resource folder

--- www/js/app.js
+++ www/js/app.js
@@ -267,3 +267,24 @@ myApp.config(function ($stateProvider, $urlRouterProvider, $ionicConfigProvider)
 	$urlRouterProvider.otherwise('/tab/home');
 
 });
+
+myApp.directive('a',function() {
+	return {
+		restrict: 'E',
+		link: function (scope, element, attrs) {
+			if ( !attrs.href ){
+				return;
+			}
+			var url = attrs.href;
+			if ( url.lastIndexOf('http',0) === 0 ){
+				element.on('click',function(e){
+					e.preventDefault();
+					if(attrs.ngClick){
+						scope.$eval(attrs.ngClick);
+					}
+					window.open(encodeURI(url), '_system');
+				});
+			};
+		}
+	}
+});
